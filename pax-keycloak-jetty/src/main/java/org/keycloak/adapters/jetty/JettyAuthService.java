511cea165818632774fb9fb46f891aa654115f13
Guillaume Nodet <gnodet@gmail.com>
2018-04-24 13:00:44 +0200

    keycloak

---
+++ pax-keycloak-jetty/src/main/java/org/keycloak/adapters/jetty/JettyAuthService.java
@@ -0,0 +1,15 @@
+package org.keycloak.adapters.jetty;
+
+import org.ops4j.pax.web.service.spi.auth.AuthenticatorService;
+
+public class JettyAuthService implements AuthenticatorService {
+
+    @Override
+    public <T> T getAuthenticatorService(String method, Class<T> iface) {
+        if ("KEYCLOAK".equals(method) && iface == org.eclipse.jetty.security.Authenticator.class) {
+            return iface.cast(new KeycloakJettyAuthenticator());
+        }
+        return null;
+    }
+
+}
